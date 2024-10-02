0c3972925b304cb1a2339ba3ca95d0edd28dbd4a
Satyam-Private <satyampatil1252@gmail.com>
2024-10-02 20:09:33 +0530

    implemented class and object creation

---
+++ class.py
@@ -0,0 +1,16 @@
+class person:
+    name = 'SAtyam'
+    occupation = 'student'
+
+#class is blueprint for creating a object
+#self is keyword used to identify the object 
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+
+a = person()
+a.name = "satyam patil"
+a.info()
+b = person()
+b.name = "s"
+b.occupation = 'HR'
+b.info()
